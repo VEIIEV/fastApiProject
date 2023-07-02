ce4d38c023b654832d5b34789c99ebfbcefc3ab1
veiiev <worldloflwarcraft@mail.ru>
2023-07-02 20:42:16 +0400

    подключил postgresql+asyncpg

---
+++ config.py
@@ -0,0 +1,38 @@
+from typing import Any, Dict, Optional
+
+from pydantic import BaseSettings, PostgresDsn, validator
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_SERVER: str
+    POSTGRES_DB: str
+
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str],
+                               values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_SERVER"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: str
+
+    class Config:
+        case_sensitive = True
+        # указываем конфигурационный файл
+        env_file = ".env"
+
+
+# через него будем обращаться к конфигурациям
+settings = Settings()
